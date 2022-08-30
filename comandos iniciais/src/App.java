40652748f7c3258d61ce6bdbe6fe1431e00d4591
William Neumann <williamrn1996@gmail.com>
2022-08-30 19:36:09 -0300

    commit inicial

---
+++ comandos iniciais/src/App.java
@@ -0,0 +1,14 @@
+/* Nomenclaturas no JAVA
+- Pacote (letra minúscula e separado por ponto se tiver várias palavras)
+- Classe (Começa com a letra maíuscula e ser um substantivo: Cor,Interruptor,Cliente,...)
+- Interface (Comeca e maiúsculo e descreve a ação que a classe faz: Runnable, Comparable, Enumerable)
+- Metodo (Começa em minúscula e a próxima palavra maiúscula, verbo: acender,desligar,...)
+- Variavel (comeca em minúsculo, não começa com caráteres especiais, recomendável não usar)
+- Constante (em maiúsculo separado por '_': MAX_PRIORITY)
+- 
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
